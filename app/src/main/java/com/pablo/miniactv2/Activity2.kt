536f63988495e3b05a596d9190f791e056a6a7cc
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-10 10:50:35 +0100

    Implemented funcionality with deprecated methods

--- app/src/main/java/com/pablo/miniactv2/Activity2.kt
+++ app/src/main/java/com/pablo/miniactv2/Activity2.kt
@@ -1,11 +1,30 @@
 package com.pablo.miniactv2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Activity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+        val number = intent.getIntExtra("NUMBER_BYE", 1)
+        val byeValue = intent.getStringExtra("BYE_TEXT")
+        var result = ""
+        if (byeValue != null) {
+            result = byeValue.repeat(number)
+        }
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val resultIntent = Intent()
+            resultIntent.putExtra("RESULT", result)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
+        val textView = findViewById<TextView>(R.id.result)
+        textView.text = result
     }
 }
--- app/src/main/java/com/pablo/miniactv2/MainActivity.kt
+++ app/src/main/java/com/pablo/miniactv2/MainActivity.kt
@@ -4,16 +4,37 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var messageTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val gotoByeScreenButton = findViewById<Button>(R.id.button)
+        val textInputBye = findViewById<EditText>(R.id.textInputEditText)
+        val numberInput = findViewById<EditText>(R.id.editTextNumber)
+
+        messageTextView = findViewById(R.id.message)
         gotoByeScreenButton.setOnClickListener{
             val activity_2 = Intent(this, Activity2::class.java)
+            // Put extra String and ints
+            activity_2.putExtra("BYE_TEXT", textInputBye.getText().toString())
+            val number: Int = Integer.parseInt(numberInput.getText().toString())
+            activity_2.putExtra("NUMBER_BYE", number)
             startActivityForResult(activity_2, 2)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode ==  2 && resultCode == RESULT_OK && data != null) {
+            val message = data.getStringExtra("RESULT")
+            messageTextView.setText(message)
+        }
+
     }
 }
