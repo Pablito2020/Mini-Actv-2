8da1db9fdec5f5d791f5a8d4d5bd97559750b11e
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-11 21:25:42 +0100

    Refactor: Use ActivityMainBinding instead of findViewById

--- app/src/main/java/com/pablo/miniactv2/MainActivity.kt
+++ app/src/main/java/com/pablo/miniactv2/MainActivity.kt
@@ -2,9 +2,6 @@ package com.pablo.miniactv2
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
 import com.pablo.miniactv2.databinding.ActivityMainBinding
@@ -13,38 +10,24 @@ import com.pablo.miniactv2.validator.MessageValidator
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var resultText: TextView
-    private lateinit var messageInput: TextView
-    private lateinit var numberInput: TextView
     private lateinit var binding: ActivityMainBinding
+
     private val onResultLauncher = registerForActivityResult(StartActivityForResult()) {
         if (it.resultCode == RESULT_OK)
-            resultText.text = it.data!!.getStringExtra(RESULT_KEY).toString()
+            binding.message.text = it.data!!.getStringExtra(RESULT_KEY).toString()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setUpTextViews()
-        setUpButton()
-    }
-
-    private fun setUpTextViews() {
-        messageInput = findViewById<EditText>(R.id.textInputEditText)
-        numberInput = findViewById<EditText>(R.id.editTextNumber)
-        resultText = findViewById(R.id.message)
-        messageInput.requestFocus()
-    }
-
-    private fun setUpButton() {
-        val gotoByeScreenButton = findViewById<Button>(R.id.button)
-        gotoByeScreenButton.setOnClickListener { validateParametersAndLaunchIntent() }
+        binding.textInputEditText.requestFocus()
+        binding.button.setOnClickListener { validateParametersAndLaunchIntent() }
     }
 
     private fun validateParametersAndLaunchIntent() {
-        val integerValidator = IntNumberValidator(numberInput.text.toString())
-        val messageValidator = MessageValidator(messageInput.text.toString())
+        val integerValidator = IntNumberValidator(binding.editTextNumber.text.toString())
+        val messageValidator = MessageValidator(binding.textInputEditText.text.toString())
         if (integerValidator.getData().isValid && messageValidator.getData().isValid)
             startNewIntent()
         else
@@ -53,8 +36,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun startNewIntent() {
         val activity2 = Intent(this, Activity2::class.java)
-        activity2.putExtra(MESSAGE_KEY, messageInput.text.toString())
-        val number: Int = Integer.parseInt(numberInput.text.toString())
+        activity2.putExtra(MESSAGE_KEY, binding.textInputEditText.text.toString())
+        val number: Int = Integer.parseInt(binding.editTextNumber.text.toString())
         activity2.putExtra(NUMBER_KEY, number)
         onResultLauncher.launch(activity2)
     }
