21ce0b5281590a877958416cbd84ad2b0dce5a74
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-13 09:35:38 +0100

    Use binding instead of findViewById on Activity2

--- app/src/main/java/com/pablo/miniactv2/Activity2.kt
+++ app/src/main/java/com/pablo/miniactv2/Activity2.kt
@@ -2,40 +2,30 @@ package com.pablo.miniactv2
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.pablo.miniactv2.databinding.Activity2Binding
 
 class Activity2 : AppCompatActivity() {
 
+    private lateinit var binding: Activity2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_2)
+        binding = Activity2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
         setUpComponents()
     }
 
     private fun setUpComponents() {
         val result = getResultFromIntent()
-        setUpButton(result)
-        setUpTextView(result)
+        binding.button.setOnClickListener { createIntentAndFinish(result) }
+        binding.result.text = result
     }
 
     private fun getResultFromIntent(): String {
         val number = intent.getIntExtra(NUMBER_KEY, DEFAULT_NUMBER)
         val textValue = intent.getStringExtra(MESSAGE_KEY)
         return textValue!!.repeat(number)
-    }
-
-    private fun setUpButton(result: String) {
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            createIntentAndFinish(result)
-        }
-    }
-
-    private fun setUpTextView(result: String) {
-        val textView = findViewById<TextView>(R.id.result)
-        textView.text = result
     }
 
     private fun createIntentAndFinish(result: String) {
